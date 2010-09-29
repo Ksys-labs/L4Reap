67d10e76e1e5d38d6624679c59e192ee897de76f
l4check <l4check@d050ee49-bd90-4346-b210-929a50b99cfc>
2010-09-29 17:06:11 +0000

    update: sync
    git-svn-id: http://svn.tudos.org/repos/oc/tudos/trunk@21 d050ee49-bd90-4346-b210-929a50b99cfc

--- l4/pkg/arm_drivers/drv/include/hw.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DRV__ARM__HW_H__
-#define __DRV__ARM__HW_H__
-
-#ifdef __cplusplus
-extern "C"
-#endif
-void hw_init(void);
-
-#endif /* ! __DRV__ARM__HW_H__ */
--- l4/pkg/arm_drivers/drv/src/integrator/hw.c
+++
@@ -1,5 +0,0 @@
-#include <l4/arm_drivers_c/hw.h>
-
-void hw_init(void)
-{
-}

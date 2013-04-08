013297c0d65e3dab85bb75c5a104babaebedd230
l4check <l4check@d050ee49-bd90-4346-b210-929a50b99cfc>
2013-04-08 17:38:05 +0000

    update: sync
    git-svn-id: http://svn.tudos.org/repos/oc/tudos/trunk@50 d050ee49-bd90-4346-b210-929a50b99cfc

--- kernel/fiasco/src/kern/arm/bsp/omap3/timer-arm-mptimer-omap4.cpp
+++
@@ -1,8 +0,0 @@
-// --------------------------------------------------------------------------
-INTERFACE [arm && mptimer && omap4_pandaboard]:
-
-EXTENSION class Timer
-{
-private:
-  static Mword interval() { return 499999; }
-};
